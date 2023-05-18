931f27fa40644c854c6859e24b15661370d0e3a6
kamite0611 <kamiteakira@gmail.com>
2023-05-18 13:14:42 +0900

    add theme

---
+++ mui/src/common/theme/globalStyles.tsx
@@ -0,0 +1,66 @@
+// @mui
+import { Noto_Sans_JP } from '@next/font/google';
+
+import { GlobalStyles as MUIGlobalStyles } from '@mui/material';
+
+// ----------------------------------------------------------------------
+
+const notoSansJp = Noto_Sans_JP({
+  weight: ['100', '300', '400', '500', '700', '900'],
+  subsets: ['latin'],
+});
+
+const FONT_FAMILY = `${notoSansJp}, sans-serif`;
+
+export default function GlobalStyles() {
+  const inputGlobalStyles = (
+    <MUIGlobalStyles
+      styles={{
+        '*': {
+          boxSizing: 'border-box',
+        },
+        html: {
+          margin: 0,
+          padding: 0,
+          width: '100%',
+          height: '100%',
+          WebkitOverflowScrolling: 'touch',
+          fontFamily: FONT_FAMILY,
+        },
+        body: {
+          margin: 0,
+          padding: 0,
+          width: '100%',
+          height: '100%',
+        },
+        '#__next': {
+          width: '100%',
+          height: '100%',
+        },
+        input: {
+          '&[type=number]': {
+            MozAppearance: 'textfield',
+            '&::-webkit-outer-spin-button': {
+              margin: 0,
+              WebkitAppearance: 'none',
+            },
+            '&::-webkit-inner-spin-button': {
+              margin: 0,
+              WebkitAppearance: 'none',
+            },
+          },
+        },
+        img: {
+          display: 'block',
+          maxWidth: '100%',
+        },
+        ul: {
+          margin: 0,
+          padding: 0,
+        },
+      }}
+    />
+  );
+
+  return inputGlobalStyles;
+}
